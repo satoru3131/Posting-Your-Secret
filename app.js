2b16d9352c28502458d94638164c434920d6275b
satoru3131 <85266699+satoru3131@users.noreply.github.com>
2023-03-26 16:47:27 +0900

    Level 1 - Encryption with a secret key

---
+++ app.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const secret = "Thisisalittleofsecret";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"]});
+
+const User = new mongoose.model("User", userSchema);
+
+
+
+
+app.get("/", function(req, res){
+    res.render("home")
+});
+
+app.get("/login", function(req, res){
+    res.render("login")
+});
+
+app.get("/register", function(req, res){
+    res.render("register")
+});
+
+
+app.post("/register", function(req, res){
+    const newUser = new User ({
+        email: req.body.username,
+        password: req.body.password 
+    })
+
+    newUser.save()
+    .then(function(){
+        res.render("secrets");
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username})
+    .then(function(foundUser){
+        if (foundUser.password === password){
+            res.render("secrets");
+        } else {
+            console.log("There is no user matched with input...");
+        };
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+})
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("Successfully the server is running on port 3000...");
+})
